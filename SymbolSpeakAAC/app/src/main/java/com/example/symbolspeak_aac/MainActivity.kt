2baa37be69de660bebb2448c79eb879544ab2e0b
Ivona Perko <ivona.perko@gmail.com>
2023-06-06 20:35:34 +0200

    Create navigation drawer

--- SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/MainActivity.kt
+++ SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/MainActivity.kt
@@ -4,40 +4,66 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.symbolspeak_aac.ui.theme.SymbolSpeakAACTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SymbolSpeakAACTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold (
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                             AppBar (
+                                 onNavigationIconClick = {
+                                     scope.launch {
+                                        scaffoldState.drawerState.open()
+                                    }
+                                 }
+                             )
+                    },
+                    drawerGesturesEnabled = scaffoldState.drawerState.isOpen,
+                    drawerContent = {
+                        DrawerHeader()
+                        DrawerBody(
+                            items = listOf(
+                                MenuItem(id = "home",
+                                title = "Home",
+                                contentDescription = "Go to home screen",
+                                icon = Icons.Default.Home),
+                                MenuItem(id = "AboutAAC",
+                                    title = "What is AAC?",
+                                    contentDescription = "Learn about AAC screen",
+                                    icon = Icons.Default.Info),
+                                MenuItem(id = "Settings",
+                                    title = "Settings",
+                                    contentDescription = "Go to settings screen",
+                                    icon = Icons.Default.Settings),
+                            ),
+                            onItemClick = {
+                                /*when(it.id) {
+                                * "settings" -> navigateToSettingsScreen
+                                * }*/
+                                println("Clicked on ${it.title}")
+                            }
+                        )
+                    }
+                ){
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SymbolSpeakAACTheme {
-        Greeting("Android")
     }
 }
