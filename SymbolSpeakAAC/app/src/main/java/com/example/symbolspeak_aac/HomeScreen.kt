7278b3170f6a94c65bb52b3359a7403edc61dbca
Ivona Perko <ivona.perko@gmail.com>
2023-07-07 19:29:42 +0200

    Create grid layout for symbols on home screen

--- SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/HomeScreen.kt
+++ SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/HomeScreen.kt
@@ -1,32 +1,61 @@
 package com.example.symbolspeak_aac
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     dataViewModel: DataViewModel = viewModel()
 ) {
     val products = dataViewModel.state.value.data
+
     products?.let {
-        LazyColumn {
-            items(
-                products
-            ) { product ->
-                Text(text = "Title: ")
-                Text(text = product.title)
-                Spacer(modifier = Modifier.padding(5.dp))
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(4),
+            contentPadding = PaddingValues(8.dp)
+        ) {
+            items(products) { product ->
+                Card(
+                    modifier = Modifier.padding(4.dp)
+                        .fillMaxWidth(),
+                    border = BorderStroke(2.dp,Color.LightGray),
+                ) {
+                    Column(
+                        modifier = Modifier.padding(1.dp),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = product.title
+                        )
+                        Image(
+                            painter = rememberAsyncImagePainter(product.imageURL),
+                            contentDescription = "gfg image",
+                            modifier = Modifier
+                                .size(60.dp)
+                                .fillMaxWidth()
+                        )
+                    }
             }
         }
     }
+
     val e = dataViewModel.state.value.e
     e?.let {
         Text(
@@ -34,4 +63,5 @@ fun HomeScreen(
         )
     }
 
+}
 }
