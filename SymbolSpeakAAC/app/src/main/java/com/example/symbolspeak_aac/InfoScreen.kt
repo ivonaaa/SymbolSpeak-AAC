142f6824737e5ee31ef314976a758dec3d51e4fb
Ivona Perko <ivona.perko@gmail.com>
2023-07-29 16:27:29 +0200

    Modify info screen

--- SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/InfoScreen.kt
+++ SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/InfoScreen.kt
@@ -10,6 +10,8 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.*
 import androidx.compose.ui.window.Popup
 import com.example.symbolspeak_aac.InfoScreenFiles.PeopleWhoNeedAACScreen
@@ -25,10 +27,18 @@ fun InfoScreen(
     var checked3 by remember { mutableStateOf(false) }
 
     Column {
+        Column(
+            modifier = Modifier
+                .padding(5.dp)
+        ) {
+            Text(text = stringResource(id = R.string.infoIntro), fontSize = 20.sp)
+        }
+
+        Column {
             TextButton(
                 onClick = { checked1 = true }
             ) {
-                Text("What is AAC?")
+                Text("* What is AAC?", fontSize = 20.sp)
             }
 
             if (checked1) {
@@ -65,7 +75,7 @@ fun InfoScreen(
 
 
             TextButton(onClick = { checked2 = true }) {
-                Text("People who need AAC")
+                Text("* People who need AAC", fontSize = 20.sp)
             }
 
             if (checked2) {
@@ -100,37 +110,38 @@ fun InfoScreen(
                 }
             }
 
-        TextButton(onClick = { checked3 = true }) {
-            Text("Types of AAC")
-        }
+            TextButton(onClick = { checked3 = true }) {
+                Text("* Types of AAC", fontSize = 20.sp)
+            }
 
-        if (checked3) {
-            Popup(
-                popupPositionProvider =
-                WindowCenterOffsetPositionProvider(),
-                onDismissRequest = { checked3 = false },
-            ) {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize(1f),
-                    border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
-                    shape = RoundedCornerShape(0.dp),
-                    color = Color.White.copy(alpha = 1.0f),
+            if (checked3) {
+                Popup(
+                    popupPositionProvider =
+                    WindowCenterOffsetPositionProvider(),
+                    onDismissRequest = { checked3 = false },
                 ) {
-                    Column(
-                        modifier = Modifier.padding(1.dp)
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize(1f),
+                        border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+                        shape = RoundedCornerShape(0.dp),
+                        color = Color.White.copy(alpha = 1.0f),
                     ) {
-                        // Composable content to be shown in the Popup
-                        Row {
-                            TextButton(onClick = { checked3 = false }) {
-                                Icon(
-                                    imageVector = Icons.Default.ArrowBack,
-                                    contentDescription = "Back"
-                                )
-                                Text(text = "Back")
+                        Column(
+                            modifier = Modifier.padding(1.dp)
+                        ) {
+                            // Composable content to be shown in the Popup
+                            Row {
+                                TextButton(onClick = { checked3 = false }) {
+                                    Icon(
+                                        imageVector = Icons.Default.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                    Text(text = "Back")
+                                }
                             }
+                            TypesOfAACScreen()
                         }
-                        TypesOfAACScreen()
                     }
                 }
             }
