210db2a70020662e718ce18de90f120205fab223
Ivona Perko <ivona.perko@gmail.com>
2023-08-06 18:27:55 +0200

    Font picker TextField->Number picker

--- SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/SettingsScreenkt.kt
+++ SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/SettingsScreenkt.kt
@@ -1,23 +1,19 @@
 package com.example.symbolspeak_aac
 
+import android.widget.NumberPicker
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.material.*
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.datastore.preferences.core.*
+import androidx.compose.ui.viewinterop.AndroidView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -32,12 +28,11 @@ fun SettingsScreen(
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun PickSettings() {
     val context = LocalContext.current
     val tokenValue2 = remember {
-        mutableStateOf(TextFieldValue())
+        mutableStateOf(16)
     }
     val store = UserSettings(context)
     val tokenText1 = store.getChosenColorSet.collectAsState(initial = false)
@@ -54,7 +49,6 @@ private fun PickSettings() {
         modifier = Modifier.padding(5.dp),
         horizontalAlignment = Alignment.Start
     ) {
-        Spacer(modifier = Modifier.height(30.dp))
 
         Text(text = tokenText1.value.toString())
         Text(text = tokenText2.value.toString())
@@ -66,42 +60,48 @@ private fun PickSettings() {
         Switch(checked = tokenValue1.value, onCheckedChange = {tokenValue1.value = it})
 
         Text(text = "Pick font size: ")
-        TextField(
-            value = tokenValue2.value,
-            onValueChange = { tokenValue2.value = it },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        AndroidView(
+            modifier = Modifier.fillMaxWidth(),
+            factory = { context ->
+                NumberPicker(context).apply {
+                    setOnValueChangedListener { numberPicker, _, _ ->
+                        tokenValue2.value = numberPicker.value
+                    }
+                    minValue = 16
+                    maxValue = 30
+                }
+            }
         )
         Text(text = "Example of ${tokenText2.value} text font size.",
             fontSize = tokenText2.value.sp)
 
         Text(text = "Pick voice rate: ")
-        Slider(
-            value = tokenValue3.value,
-            onValueChange = { sliderValue_ ->
-                tokenValue3.value = sliderValue_
-            },
-            onValueChangeFinished = {
-                // this is called when the user completed selecting the value
 
-            },
-            valueRange = 0f..1f
-        )
+        Column(
+            modifier = Modifier.padding(10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
-        Text(text = tokenValue3.value.toString())
+            Slider(
+                value = tokenValue3.value,
+                onValueChange = { sliderValue_ ->
+                    tokenValue3.value = sliderValue_
+                },
+                onValueChangeFinished = {
+                    // this is called when the user completed selecting the value
 
+                },
+                valueRange = 0f..1f
+            )
+            Text(text = tokenValue3.value.toString())
+        }
         Spacer(modifier = Modifier.height(30.dp))
 
         Button(
             onClick = {
                 CoroutineScope(Dispatchers.IO).launch {
-                    if (tokenValue2.value.text.toInt() < 16) {
-                        tokenValue2.value = TextFieldValue("16")
-                    }
-                    if (tokenValue3.value < 0.0 || tokenValue3.value > 1.0) {
-                        tokenValue3.value = 1.0F
-                    }
                     store.saveToken(tokenValue1.value,
-                        tokenValue2.value.text.toInt(),
+                        tokenValue2.value,
                         tokenValue3.value)
                 }
             }
