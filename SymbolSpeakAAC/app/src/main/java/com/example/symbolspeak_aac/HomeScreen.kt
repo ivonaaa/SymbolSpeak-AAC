2123a653dc11b2c73a1f6332f4abe19a3e2c2390
Ivona Perko <ivona.perko@gmail.com>
2023-08-21 12:17:56 +0200

    Groupe symbols on home screen

--- SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/HomeScreen.kt
+++ SymbolSpeakAAC/app/src/main/java/com/example/symbolspeak_aac/HomeScreen.kt
@@ -1,7 +1,7 @@
 package com.example.symbolspeak_aac
 
 import androidx.compose.foundation.BorderStroke
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
@@ -15,12 +15,17 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
 import com.example.symbolspeak_aac.ChosenSymbolsFiles.ChosenSymbolView
 import com.example.symbolspeak_aac.ChosenSymbolsFiles.ChosenSymbols
 import com.example.symbolspeak_aac.Firebase.DataViewModel
@@ -28,7 +33,9 @@ import com.example.symbolspeak_aac.History.History
 import com.example.symbolspeak_aac.History.HistoryScreen
 import com.example.symbolspeak_aac.InfoScreenFiles.WindowCenterOffsetPositionProvider
 import com.example.symbolspeak_aac.SettingsScreenFiles.UserSettings
+import com.example.symbolspeak_aac.Symbol.Symbol
 import com.example.symbolspeak_aac.Symbol.SymbolView
+import com.example.symbolspeak_aac.Symbol.colorPicker
 import com.example.symbolspeak_aac.TextToSpeach.TextToSpeechViewModel
 
 
@@ -90,6 +97,7 @@ fun HomeScreen(
                 }
             }
         }
+
         Row(
             modifier = Modifier
                 .padding(5.dp)
@@ -142,14 +150,21 @@ fun HomeScreen(
                 }
             }
         }
-
+        
         products?.let {
+            val gruped = products.groupBy { it.type }
+            val types = mutableStateListOf<String>()
+            gruped.forEach {
+                types.add(it.key)
+            }
             LazyVerticalGrid(
                 columns = GridCells.Fixed(3),
                 contentPadding = PaddingValues(8.dp)
             ) {
-                items(products.size) {
-                    SymbolView(product = products[it], chosenSymbols)
+                items(gruped.size) {
+                    val type = types[it]
+                    val symbols = gruped.getValue(type)
+                    ShowGroup(type = type, symbols = symbols, chosenSymbols = chosenSymbols)
                 }
             }
         }
@@ -160,5 +175,94 @@ fun HomeScreen(
                 text = e.message!!
             )
         }
+    }
+}
+
+@Composable
+fun ShowGroup(
+    type: String,
+    symbols: List<Symbol>,
+    chosenSymbols: ChosenSymbols
+) {
+    var show by remember {
+        mutableStateOf(false)
+    }
+
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth(),
+        border = BorderStroke(3.dp, color = colorPicker(type = type)),
+    ) {
+        Button(
+            onClick = { show = true },
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color.White),
+            modifier = Modifier.height(100.dp)
+        ) {
+            Grupe(type = type, image = symbols[0].imageURL)
+            if (show) {
+                Popup(
+                    onDismissRequest = { show = false },
+                    alignment = Alignment.TopCenter,
+                    properties = PopupProperties()
+                    ) {
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height((LocalConfiguration.current.screenHeightDp-220).dp),
+                        border = BorderStroke(1.dp, MaterialTheme.colors.secondary),
+                        shape = RoundedCornerShape(0.dp),
+                        color = MaterialTheme.colors.background,
+                    ) {
+                        Column(
+                            modifier = Modifier.padding(1.dp)
+                        ) {
+                            // Composable content to be shown in the Popup
+                            Row {
+                                TextButton(onClick = { show = false }) {
+                                    Icon(
+                                        imageVector = Icons.Default.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                    Text(text = "Back", fontSize = 20.sp)
+                                }
+                            }
+                            LazyVerticalGrid(
+                                columns = GridCells.Fixed(3),
+                                contentPadding = PaddingValues(8.dp)
+                            ) {
+                                items(symbols.size) {
+                                    SymbolView(product = symbols[it], chosenSymbols)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Grupe(
+    type: String,
+    image: String
+) {
+    Column(
+        modifier = Modifier.padding(1.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = type
+        )
+        Image(
+            painter = rememberAsyncImagePainter(image),
+            contentDescription = "product image",
+            modifier = Modifier
+                .padding(2.dp)
+                .size(60.dp)
+                .fillMaxWidth()
+        )
     }
 }
